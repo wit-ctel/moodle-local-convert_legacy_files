ef63ea4933dc249bac366ab9d0ab7125013c6140
Viettrung Luong <yuezhongl@gmail.com>
2015-09-28 10:59:04 +0100

    Work in progress, needs to sort out the loops where same link crops up mulitple times in an area.

--- move_legacy_files.php
+++ move_legacy_files.php
@@ -19,7 +19,7 @@ abstract class MoveLegacyFilesAbstract
      * @param string $activity_area Activity Area
      * @param string $found_filearea Found Filearea
      */
-    abstract function build_link_record($old_url, $new_url, $activity, $activity_area, $found_filearea);
+    abstract function build_link_record($old_url, $new_url, $activity, $activity_area, $found_filearea, $positions_array);
 
     /* Run SQL query for list of activities with legacy files.
      * @param string $table sql table
@@ -38,105 +38,146 @@ abstract class MoveLegacyFilesAbstract
         return($results);
     } // End search_activity
 
-
     /* Search activity for legacy files.
      * @param array $sql_results sql results
      * @param string $activity_name activity name
-     * @param string $area activity area
+     * @param string $activity_area activity area
      */
-    public function search_activity_files($sql_results, $activity_name, $area)
+    public function search_activity_files($sql_results, $activity_name, $activity_area)
     {
         foreach($sql_results as $key=>$activity)
         {
             $matches[] = '';
             // Find where file.php pops up
             $regexp = "\/file.php\/([^\"]*)\"";
-            preg_match_all("/$regexp/siU", $activity->{$area}, $matches, PREG_SET_ORDER);
+            preg_match_all("/$regexp/siU", $activity->{$activity_area}, $matches, PREG_OFFSET_CAPTURE);
 
-            foreach($matches as $match)
+            for($i=1; $i < count($matches); $i++)
             {
-                // Trim out anything after '?'
-		$matched = (explode('?', $match[1]));
-                $search_match = array_shift($matched);
-                echo "Matched Search: " . $search_match . "\n";
+                // Iterate through each match and group them together into a results table
+                $result = array();
+                foreach($matches[$i] as $match)
+                {
+                    $relative_path = $match[0];
+                    if(isset($result[$relative_path]))
+                    {
+                        // Append to an existing match array
+                        $result[$relative_path][] = $match;
+                    }
+                    else
+                    {
+                        // Create a new match array
+                        $result[$relative_path] = array($match);
+                    }
+                }
 
-                // Get the relative url
-                $relative_url = '/file.php/' . $search_match;
+                // Process each of the results in the table
+                foreach($result as $relative_path=>$positions_array)
+                {
+                    // Get the relative url and clean the filename
+                    $relative_url = '/file.php/' . $relative_path;
+                    $filename = urldecode(basename($relative_path));
+                    $filename = strtok($filename, "?");
 
-                // Get filename
-                $filename = basename($search_match);
-                echo "Found file: " . $filename . "\n";
-                echo "Relative URL: " . $relative_url . "\n";
+                    echo "Found file: " . $filename . "\n";
+                    echo "Relative URL: " . $relative_url . "\n";
 
-                // Call function to move the file to mod activity
-                $this->move_legacy_file_to_activity($search_match, $relative_url, $filename, $activity, $activity_name, $area);
+                    // Call function to move file and add links.
+                    $this->move_legacy_file_to_activity($relative_path, $relative_url, $filename, $activity, $activity_name, $activity_area, $positions_array);
+                }
             }
         }
     } // End search_activity_files
 
 
     /* Move Legacy file to activity area
-     * @param string $relativepath relative path
+     * @param string $relative_path relative path
      * @param string $relative_url relative url
      * @param string $filename file name
      * @param array $activity activity
      * @param string $activity_name activity name
      * @param string $activity_area activity area
      */
-    private function move_legacy_file_to_activity($relativepath, $relative_url, $filename, $activity, $activity_name, $activity_area)
+    private function move_legacy_file_to_activity($relative_path, $relative_url, $filename, $activity, $activity_name, $activity_area, $positions_array)
     {
-	global $DB;
-	
+        global $DB;
+
         // Build the pathnamehash to locate the legacy file
-        $args = explode('/', $relativepath);
+        $args = explode('/', $relative_path);
         $rpath = array_slice($args, 1);
         $final_path = implode('/', $rpath);
-
-        // Retrieve the file and store it temporarily
         $context = context_course::instance($activity->course);
+        $fullpath = "/" . $context->id . "/course/legacy/0/" . urldecode($final_path);
+        $legacy_file_pathnamehash = sha1(strtok($fullpath, "?"));
+        echo $legacy_file_pathnamehash ."\n";
+
+        // Prepare legacy file storage
         $fs = get_file_storage();
-        $fullpath = "/" . $context->id . "/course/legacy/0/" . $final_path;
 
-	echo "Fullpath: " . $fullpath . "\n";
+        // Check if file have already between moved from a different area
+        $areas = array( "intro",
+                "content"
+        );
 
-        //Check for instance of file via filename, ignoring draftfiles
-	$file_instance = $DB->get_record_sql('SELECT * FROM {files} WHERE pathnamehash LIKE ? AND filearea <> ? ', array(sha1($fullpath), 'draft'));
-        
-	// Check which area of the activity that the file is in
-        switch ($file_instance['filearea'])
+        foreach($areas as $area)
         {
-	    case 'intro':
-	    case 'content':
-		// Code block for matching intro or content
-		echo "File already exists in non-legacy area: " . $file_instance->filearea . " : Updating link to match...\n";
-		$old_file = $fs->get_file_by_hash($file_instance['pathnamehash']);
-		
-		// Call the function to build the file record
-		$file_record = $this->build_file_record($old_file, $context, $activity_name, $activity_area);
-		$this->update_file_link($relative_url, $activity, $file_record, $activity_name, $activity_area, $file_instance['filearea']);
-		break;
-		// End code block
-	    case 'legacy':
-		// Code Block for matching legacy
-		echo "Found Legacy file, relocating...\n";
-		$old_file = $fs->get_file_by_hash($file_instance['pathnamehash']);
-		
-		// Call the function to build the file record
-		$file_record = $this->build_file_record($old_file, $context, $activity_name, $activity_area);
-		
-		// Create the file in the new location
-		$sf = $fs->create_file_from_storedfile($file_record, $old_file);
-		echo "Copied file with pathnamehash: " . $file_instance['pathnamehash'] . " to new location with new pathnamehash:" . $sf->get_pathnamehash() . "\n";
-		$this->update_file_link($relative_url, $activity, $file_record, $activity_name, $activity_area, $file_instance['filearea']);
-		$this->remove_legacy_file($old_file);
-		break;
-		// End code block
-	    default:
-		echo "File not found in expected filearea of intro, content or legacy: " . $file_instance['filearea'] . " Skipping...\n\n";
+            //Check for instance of file via filename, ignoring draftfiles
+            $file_instance = $DB->get_record_sql('SELECT * FROM {files} WHERE filename LIKE ? AND filearea = ? AND component = ? ', array($filename, $area, "mod_" . $activity_name));
+
+            if(!$file_instance)
+            {
+                echo "Nothing found under $area \n";
+                $found_filearea[$area] = 0;
+            }
+            else
+            {
+                echo "Found existing file under $area \n";
+                $found_filearea[$area] = 1;
+            }
         }
+
+        // Double check if legacy file actually exists, sometimes there's a typo!
+        if($fs->file_exists_by_hash($legacy_file_pathnamehash))
+        {
+            // Check if the file has already been moved to an existing area
+            if($found_filearea['intro'] || $found_filearea['content'] !== 1)
+            {
+            // Proceeding with moving leagacy file coz nothing found in either area
+            $old_file = $fs->get_file_by_hash($legacy_file_pathnamehash);
+            $old_url = '';
+
+            // Call the function to build the file record
+            $file_record = $this->build_file_record($old_file, $context, $activity_name, $activity_area);
+//            $link_record = $this->build_link_record($old_url, $relative_path, $activity, $activity_area, $found_filearea, $positions_array);
+
+            // Create the file in the new location
+            $sf = $fs->create_file_from_storedfile($file_record, $old_file);
+            echo "Copied file with pathnamehash: " . $legacy_file_pathnamehash . " to new location with new pathnamehash:" . $sf->get_pathnamehash() . "\n";
+            // Update all the file links
+            $this->update_file_link($relative_url, $activity, $file_record, $activity_name, $activity_area, "legacy", $positions_array);
+            //$this->remove_legacy_file($old_file);
+            }
+            else
+            {
+                echo "Re-linking...\n";
+                foreach($areas as $area)
+                {
+                    if($found_filearea[$area] === 1)
+                    {
+                        $this->update_file_link($relative_url, $activity, $file_record, $activity_name, $activity_area, $area, $positions_array);
+                    }
+
+                }
+            }
+        }
+        else
+        {
+            echo "Missing legacy file, skipping...\n";
+        }
+
     } // End move_leagcy_file_to_activity
 
-    
+
     /* Delete Legacy file
      * @param stdClass $fs file storage
      * @param stdClass $old_file legacy file
@@ -151,7 +192,7 @@ abstract class MoveLegacyFilesAbstract
         }
     } // End remove_legacy_file
 
-    
+
     /* Update link
      * @param string $old_path old path
      * @param array $activity activity
@@ -159,26 +200,26 @@ abstract class MoveLegacyFilesAbstract
      * @param string $activity_name
      * @param string $activity_area
      */
-    private function update_file_link($old_path, $activity, $file, $activity_name, $activity_area, $found_filearea)
+    private function update_file_link($old_path, $activity, $file, $activity_name, $activity_area, $found_filearea, $positions_array)
     {
         global $DB, $CFG;
 
-        // Build new URL
-        $new_url = $CFG->wwwroot . '/pluginfile.php/' . $file->contextid . '/' . $file->component . '/' . $file->filearea . $file->filepath . $file->filename;
+        // Build new replacement string
+        $new_path = '/pluginfile.php/' . $file->contextid . '/' . $file->component . '/' . $file->filearea . $file->filepath . $file->filename;
         $old_url = $CFG->wwwroot . $old_path;
 
-        echo "New URL: " . $new_url . "\n";
+        echo "New path: " . $new_path . "\n";
 
         /* Call function to build the link record to be saved to DB.
          * The build_link_record function is to be overriden by the relevant class.
          */
-        $link_record = $this->build_link_record($old_url, $new_url, $activity, $activity_area, $found_filearea);
+        $link_record = $this->build_link_record($old_url, $new_path, $activity, $activity_area, $found_filearea, $positions_array);
 
         // Update DB with new record
         $DB->update_record($activity_name, $link_record, false);
     } // End update_file_link
 
-    
+
     /* Build the file record for saving to the DB
      * @param string $filename file name
      * @param array $context context
@@ -199,7 +240,7 @@ abstract class MoveLegacyFilesAbstract
         $file_record->author = $file->get_author();
         $file_record->license = $file->get_license();
         $file_record->filename = $file->get_filename();
-        $file_record->mimetype = $file->get_mimetype();        
+        $file_record->mimetype = $file->get_mimetype();
         $file_record->source = '';
         $file_record->timecreated = time();
         $file_record->timemodified = time();
@@ -212,7 +253,7 @@ abstract class MoveLegacyFilesAbstract
 // Move Legacy Label Files Class
 class MoveLegacyLabelFiles extends MoveLegacyFilesAbstract
 {
-    public function build_link_record($old_url, $new_url, $label, $activity_area, $found_filearea)
+    public function build_link_record($old_url, $new_url, $label, $activity_area, $found_filearea, $positions_array)
     {
         $updated_content = str_replace($old_url, $new_url, $label->{$activity_area});
 
@@ -234,7 +275,7 @@ class MoveLegacyLabelFiles extends MoveLegacyFilesAbstract
 // Move Legacy Assignment Files class
 class MoveLegacyAssignmentFiles extends MoveLegacyFilesAbstract
 {
-    public function build_link_record($old_url, $new_url, $assign, $activity_area, $found_filearea)
+    public function build_link_record($old_url, $new_url, $assign, $activity_area, $found_filearea, $positions_array)
     {
         $updated_content = str_replace($old_url, $new_url, $assign->{$activity_area});
 
@@ -256,17 +297,25 @@ class MoveLegacyAssignmentFiles extends MoveLegacyFilesAbstract
 // Move Legacy Page Files class
 class MoveLegacyPageFiles extends MoveLegacyFilesAbstract
 {
-    public function build_link_record($old_url, $new_url, $page, $activity_area, $found_filearea)
+    public function build_link_record($old_url, $new_url, $page, $activity_area, $found_filearea, $positions_array)
     {
-        // Check if the there's already a file plus updated link in intro area. If so, just copy
-        if($found_filearea === 'intro' && $activity_area === 'content')
+        foreach($positions_array as $positions)
         {
-            echo "Found file in another file area: " . $found_filearea . "\n";
-            $new_url = str_replace($activity_area, $found_filearea, $new_url);
-	    echo "Updating URL to existing one: " . $new_url . "\n\n";
+//          // Check if the there's already a file plus updated link in intro area. If so, just copy link
+//          if($found_filearea === 'intro' && $activity_area === 'content')
+//          {
+//              echo "Found file in another file area: " . $found_filearea . "\n";
+//              $new_url = str_replace($activity_area, $found_filearea, $new_url);
+//              echo "Updating URL to existing one: " . $new_url . "\n\n";
+//          }
+
+            $position = $positions[1] - strlen('/file.php/');
+            echo "Position: $position \n";
+            $updated_area = substr_replace($page->{$activity_area}, $new_url, $position, strlen($new_url));
         }
 
-        $updated_area = str_replace($old_url, $new_url, $page->{$activity_area});
+        echo $updated_area . "\n\n";
+
         $page->{$activity_area} = $updated_area;
 
         $record = new stdClass();
@@ -286,15 +335,15 @@ class MoveLegacyPageFiles extends MoveLegacyFilesAbstract
 // Start the Script
 function start()
 {
-    echo "Searching Labels for Legacy Files...\n";
-    $labelFiles = new MoveLegacyLabelFiles();
-    $sql_results = $labelFiles->search_activity('label', 'intro');
-    $labelFiles->search_activity_files($sql_results, 'label', 'intro');
+//     echo "Searching Labels for Legacy Files...\n";
+//     $labelFiles = new MoveLegacyLabelFiles();
+//     $sql_results = $labelFiles->search_activity('label', 'intro');
+//     $labelFiles->search_activity_files($sql_results, 'label', 'intro');
 
-    echo "Searching Assignments for Legacy Files...\n";
-    $assignFiles = new MoveLegacyAssignmentFiles();
-    $sql_results = $assignFiles->search_activity('assign', 'intro');
-    $assignFiles->search_activity_files($sql_results, 'assign', 'intro');
+//     echo "Searching Assignments for Legacy Files...\n";
+//     $assignFiles = new MoveLegacyAssignmentFiles();
+//     $sql_results = $assignFiles->search_activity('assign', 'intro');
+//     $assignFiles->search_activity_files($sql_results, 'assign', 'intro');
 
     $pageFiles = new MoveLegacyPageFiles();
     echo "Searching Intro Area of Pages for Legacy Files...\n";
